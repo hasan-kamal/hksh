e50baf79b58902d67a18d1db1eede81736a74aed
Hasan Kamal <hasankamal007@gmail.com>
2019-07-15 19:53:28 +0530

    added source in src directory

---
+++ src/built_in_commands.c
@@ -0,0 +1,59 @@
+/*
+
+@author Hasan Kamal
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include "history.c"
+#define NUM_BUILT_IN_COMMANDS 4
+#define BUILT_IN_COMMAND_STR_LEN 10
+#define MAX_HELP_DESCRIPTION_LEN 100
+
+// structure to encapsulate a built-in command
+typedef struct{
+	char command_name[BUILT_IN_COMMAND_STR_LEN];
+	void (*func)(char *command);
+	char description_help[MAX_HELP_DESCRIPTION_LEN];
+} built_in_command;
+
+void help(char *command);
+void cd(char *command);
+void history(char *command);
+void exit_builtin(char *command);
+
+// list of all the available built-in commands
+built_in_command built_ins[NUM_BUILT_IN_COMMANDS] = {
+	{"help", &help, "lists commands available in this shell"},
+	{"cd", &cd, "change current directory"},
+	{"history", &history, "show history of previously entered commands"},
+	{"exit", &exit_builtin, "exit the shell"}
+};
+
+void help(char *command){
+	printf("\nhelp--\n");
+	for(int i=0; i<NUM_BUILT_IN_COMMANDS; i++)
+		printf("%d-%s : %s\n", i+1, built_ins[i].command_name, built_ins[i].description_help);
+	printf("\n");
+}
+
+void cd(char *command){
+	char *cmd = strdup(command);
+	cmd = strtok(cmd, " ");
+	cmd = strtok(NULL, " ");
+
+	if(chdir(cmd)==-1){
+		printf("error in cd\n");
+	}
+}
+
+void history(char *command){
+	//char *cmd = strdup(command);
+	display_history();
+}
+
+void exit_builtin(char *command){
+	save_history();
+	exit(0);
+}
---
+++ src/history.c
@@ -0,0 +1,93 @@
+/*
+
+@author Hasan Kamal
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#define FILENAME "history_hksh.txt"
+#define MAX_COMMAND_LEN 50
+#define MAX_PATH_LENGTH 100
+
+char file_path[MAX_PATH_LENGTH];
+
+struct node{
+	char *command_entry;
+	struct node *next;
+};
+
+typedef struct node entry;
+
+entry *front, *tail;
+
+void add_entry_history(char *command){
+	entry *new_entry = malloc(sizeof(entry));
+	new_entry->command_entry = strdup(command);
+	new_entry->next = NULL;
+
+	if(front==NULL){
+		front = tail = new_entry;
+	}else{
+		tail->next = new_entry;
+		tail = new_entry;
+	}
+}
+
+void display_history(){
+	entry *t = front;
+	int i=1;
+	while(t){
+		printf("%d: %s\n", i, t->command_entry);
+		t = t->next;
+		i++;
+	}
+}
+
+void restore_history(){
+	
+	front = tail = NULL;
+
+	FILE *file = fopen(FILENAME, "r");
+	getcwd(file_path, MAX_PATH_LENGTH);
+	if(file==NULL){
+		fopen(FILENAME, "w");	
+		return;
+	}
+	
+	char input[MAX_COMMAND_LEN];
+	while(fgets(input, MAX_COMMAND_LEN, file)){
+		input[strlen(input)-1] = '\0';
+		add_entry_history(input);
+	}
+
+	fclose(file);
+}
+
+void save_history(){
+
+	if(chdir(file_path)==-1){
+		printf("couldn't save history successfully\n");
+	}
+	FILE *file = fopen(FILENAME, "w");
+
+	entry *t = front;
+	while(t){
+		fputs(t->command_entry, file);
+		fputs("\n", file);
+		t = t->next;
+	}
+
+	fclose(file);
+
+	// free each node
+	t = front;
+	while(t){
+		free(t);
+		t = t->next;
+	}
+
+	front = tail = NULL;
+}
